d59bf113bb286ee7548215a2d57aac29e660d884
Salfay Anna Rita <salfay.anna@gmail.com>
2019-03-28 16:16:16 +0100

    feat(Hi5): testbutton added

---
+++ FE/src/app/components/teszt-main-page/teszt-main-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teszt-main-page',
+  templateUrl: './teszt-main-page.component.html',
+  styleUrls: ['./teszt-main-page.component.scss']
+})
+export class TesztMainPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
