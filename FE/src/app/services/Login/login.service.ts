a80e5272c7f6fb11c9a2dada5e1b63fcd814e353
Salfay Anna Rita <salfay.anna@gmail.com>
2019-03-28 17:35:08 +0100

    feat(Hi5): unused req removed

--- FE/src/app/services/Login/login.service.ts
+++ FE/src/app/services/Login/login.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { throwError, Observable, of } from 'rxjs';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
-import { ErrorHandlingService } from '../ErrorHandlingService/error-handling.service';
+import { ErrorHandlingService } from '../ErrorHandling/error-handling.service';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
@@ -21,7 +21,7 @@ export class LoginService {
       'username': `${username}`,
       'password': `${password}`
     };
-    console.log(body)
+    
     const header = {
       headers: new HttpHeaders
         ({
@@ -30,21 +30,6 @@ export class LoginService {
         })
     };
     return this.http.post<any>(url, body, { headers: {  'Content-Type': 'application/json',  'Accept': 'application/json'} })
-      .pipe(catchError((e) => this.errorHandler(e)));
-  }
-
-  excuse(token:string): Observable<Object> {
-    const url = this.baseUrl + `/execuse`;
-  
-    const header = {
-      headers: new HttpHeaders
-        ({
-          'Content-Type': 'application/json',
-          'X-Requested-With': 'XMLHttpRequest',
-          'token': `Bearer ${token}`
-        })
-    };
-    return this.http.get<any>(url, header)
       .pipe(catchError((e) => this.errorHandler(e)));
   }
 
