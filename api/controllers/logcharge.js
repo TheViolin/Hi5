0da56e622e6064e22130113e4db2080e11e74a35
Andras Gerencser <andras.gerencser@gmail.com>
2019-03-29 11:12:46 +0100

    fix(): first logcharge

--- api/controllers/logcharge.js
+++ api/controllers/logcharge.js
@@ -26,23 +26,47 @@ exports.getLog = (req, res, next) => {
 }
 
 exports.logCharge = (req, res, next) => {
-  const log = new Log({
-    _id: mongoose.Types.ObjectId(),
-    user: req.userData.userId,
-    odometer: req.body.odometer,
-    chargeAmount: req.body.chargeAmount,
-    unitPrice: req.body.unitPrice
-  });
-  log.save()
-    .then(result => {
-      res.status(201).json({
-        message: 'Charging logged successfully.'
+  Log.find({ user: req.userData.userId })
+    .exec()
+    .then(logs => {
+      const log = new Log({
+        _id: mongoose.Types.ObjectId(),
+        user: req.userData.userId,
+        odometer: req.body.odometer,
+        chargeAmount: req.body.chargeAmount,
+        unitPrice: req.body.unitPrice
       });
+      if (logs.length = 1 && req.body.odometer < 500) {
+        log.save()
+          .then(result => {
+            res.status(201).json({
+              message: 'Charging logged successfully.'
+            });
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).json({
+              error: err
+            });
+          }); 
+      } else {
+        Log.findOneAndUpdate({ user: req.userData.userId }, { $set: { 
+          odometer: req.body.odometer,
+          chargeAmount: req.body.chargeAmount,
+          unitPrice: req.body.unitPrice }
+        }, (err, log) => {
+          if (err) {
+            reject(err);
+          } else {
+            fulfill(log);
+          }
+        });
+      }
     })
     .catch(err => {
       console.log(err);
       res.status(500).json({
         error: err
       });
-    }); 
-}
+    });
+  }
