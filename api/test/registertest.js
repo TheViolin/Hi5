4595be10c9bdc152568a8cc64aed2002b2e32e4b
Juli Domány <domanyjuli@gmail.com>
2019-03-29 06:43:46 +0100

    feat(test): register

---
+++ api/test/registertest.js
@@ -0,0 +1,47 @@
+const request = require('supertest');
+const { expect } = require('chai');
+const app = require('../../app');
+
+const user = {
+  username: 'test12',
+  password: 'test',
+};
+
+describe('POST /User/Register', () => {
+  it('should get a status code \'201\' if user registered successfully', (done) => {
+    request(app)
+      .post('/user/register')
+      .send(user)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .end((err, res) => {
+        expect(res.body).to.be.an('object')
+          .with.property('status')
+          .that.is.a('string')
+          .that.equal('ok');
+        if (err) return done(err);
+        return done();
+      });
+      done()
+  });
+
+  //token
+  it('should get a status code \'409\' if user exists', (done) => {
+    request(app)
+      .post('/user/login')
+      .send(user)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(409)
+      .end((err, res) => {
+        expect(res.body).to.be.an('object')
+          .with.property('status')
+          // .that.is.a('string')
+          .that.equal('username already exists.');
+        if (err) return done(err);
+        return done();
+      });
+    done()
+  })
+})
